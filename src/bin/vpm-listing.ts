9f4b145601918bac59f27ba82cb91311ba641c18
Narazaka <info@narazaka.net>
2025-01-23 04:52:49 +0900

    breaking: -S, --no-calc-sha256

--- src/bin/vpm-listing.ts
+++ src/bin/vpm-listing.ts
@@ -13,7 +13,7 @@ const program = new Command()
   )
   .option("-i, --input <input>", "Your source.json", "source.json")
   .option("-o, --output <output>", "Output index.json", "index.json")
-  .option("-s, --calc-sha256", "Generate zipSHA256", true as boolean)
+  .option("-S, --no-calc-sha256", "Do not generate zipSHA256", true as boolean)
   .option("-c, --concurrency <num>", "Concurrency", (arg) => {
     const num = Number(arg);
     if (Number.isNaN(num) || num < 1) {
