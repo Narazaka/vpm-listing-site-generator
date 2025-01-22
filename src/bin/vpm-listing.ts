9d5cf8bc4934fa344e13786fe492da916032e0e9
Narazaka <info@narazaka.net>
2025-01-23 04:55:49 +0900

    desc

--- src/bin/vpm-listing.ts
+++ src/bin/vpm-listing.ts
@@ -14,7 +14,7 @@ const program = new Command()
   .option("-i, --input <input>", "Your source.json", "source.json")
   .option("-o, --output <output>", "Output index.json", "index.json")
   .option("-S, --no-calc-sha256", "Do not generate zipSHA256", true as boolean)
-  .option("-K, --no-check", "Check output format", true as boolean)
+  .option("-K, --no-check", "Do not check output format", true as boolean)
   .option("-c, --concurrency <num>", "Concurrency", (arg) => {
     const num = Number(arg);
     if (Number.isNaN(num) || num < 1) {
